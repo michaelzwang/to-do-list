0a4342c59dfc14324455a89a29351466f24e22eb
Michael Wang <michaelzwang92@gmail.com>
2016-09-20 16:09:00 -0700

    create and style list item

---
+++ resources/js/main.js
@@ -0,0 +1,8 @@
+
+// User clicked on the add button
+document.getElementbyId('add').addEventListener('click', function() {
+  var value = document.getElementById('item').value;
+  if (value) {
+    
+  }
+});
